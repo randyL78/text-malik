434f439dbe5f771a401d410e89fda08cf175f878
Randy's Mac <randydavidl78@gmail.com>
2019-01-11 08:30:11 -0500

    update .gitignore and add ch3 examples 13 - 15

---
+++ ch-3/examples/example-14.cpp
@@ -0,0 +1,34 @@
+// This program illustrates how the function setfill works
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string name = "Jessica";
+  double gpa = 3.75;
+  int scholarship = 7850;
+
+  cout << "123456789012345678901234567890" << endl;
+  cout << fixed << showpoint << setprecision(2);
+
+  cout << setw(10) << name << setw(7) << gpa << setw(8) << scholarship << endl;
+
+  cout << setfill('*');
+  cout << setw(10) << name << setw(7) << gpa << setw(8) << scholarship << endl;
+
+  cout << setw(10) << name << setfill('#') << setw(7) 
+       << gpa << setw(8) << scholarship << endl;
+
+  cout << setw(10) << setfill('@') << name 
+       << setw(7) << setfill('#') << gpa 
+       << setw(8) << setfill('^') << scholarship << endl;
+
+  cout << setfill(' ');
+  cout << setw(10) << name << setw(7) << gpa << setw(8) << scholarship << endl;
+
+
+  return 0;
+}
