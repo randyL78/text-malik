fa2916cd6656cb06068236244a810e4983617a83
Randy's Mac <randydavidl78@gmail.com>
2019-01-11 17:15:02 -0500

    add student_grade.cpp file

---
+++ ch-3/programming_examples/student_grade.cpp
@@ -0,0 +1,9 @@
+// *******************************************************
+// Author: Randy Layne
+//
+// Program: Student Grade
+// Description: Program to caclulate the average test score.
+// Given a student's name and 5 test scores, this program
+// calculates the average test score. The student's name, the
+// five test scores, and the average test score are stored in 
+// the file testavg.out. The data is input from the file test.txt
