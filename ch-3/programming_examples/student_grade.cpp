c092096b92b778e1118179c4263cfaef16634edb
Randy Layne <randylayne@Randys-MacBook-Air.local>
2019-01-12 16:39:33 -0500

    complete student_grade.cpp

--- ch-3/programming_examples/student_grade.cpp
+++ ch-3/programming_examples/student_grade.cpp
@@ -7,3 +7,39 @@
 // calculates the average test score. The student's name, the
 // five test scores, and the average test score are stored in 
 // the file testavg.out. The data is input from the file test.txt
+
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    // declare variables
+    ifstream inFile;
+    ofstream outFile;
+    
+    double test1, test2, test3, test4, test5;
+    double average;
+
+    string firstName;
+    string lastName;
+
+    inFile.open("./test.txt");
+    inFile >> firstName >> lastName >> test1 >> test2 >> test3
+           >> test4 >> test5;
+
+    average = (test1 + test2 + test3 + test4 + test5) / 5;
+
+    outFile.open("./testavg.out");
+    outFile << firstName << " " << lastName << ' ' << average;
+
+
+    inFile.close();
+    outFile.close();
+
+    return 0;
+}
