28e5f2cbda44c103ad3f05e57bd5cc29bf8e3ac5
Randy Layne <randydavidl78@gmail.com>
2019-05-21 12:04:54 -0400

    Begin ch 6 Cable Co program

---
+++ ch-6/cable-company-billing.cpp
@@ -0,0 +1,20 @@
+// *************************************************************
+// Author: D. S. Malik, modified by: Randy Layne
+// 
+// Program: Cable Company billing
+// This program calculates and prints a customer's bill for
+// a local cable company
+// *************************************************************
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// named constants - residential customers
+const double RES_BILL_PROC_FEE = 4.50;
+const double RES_BASIC_FEE = 20.50;
+const double RES_COST_PREM_CHANNEL = 7.50;
+
+
+// named constants - commercial customers
